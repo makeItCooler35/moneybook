4cd5fe2bb644a7bd9d397702000375cd281e9873
Никита Богословский <nik35is@yandex.ru>
2023-07-28 13:15:40 +0300

    создан комопнент nfileinput

---
+++ frontend/src/components/NFileInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <b-container>
+    <b-file
+      v-model="file"
+      :state="Boolean(file)"
+      :placeholder="placeholder"
+      :accept="acceptedFormats.join(',')"
+      @input="OnChange"
+    />
+  </b-container>
+</template>
+
+<script>
+
+export default({
+  name: 'NFileInput',
+  props: {
+    acceptedFormats: {type: Array, default: () => []},
+    value: {required: true}
+  },
+  data(){
+    return{
+      placeholder: 'Выберите файл',
+    };
+  },
+  computed: {
+    file: {
+      get() {
+        return this.value;
+      },
+      set(val) {
+        this.$emit('input', val);
+      }
+    }
+  },
+  methods: {
+    OnChange(item) {
+      if(!(this.acceptedFormats.indexOf(item.type) > -1)) {
+        this.file = null;
+        this.placeholder = 'Неверный формат';
+      }
+      else {
+        this.placeholder = item.name;
+      }
+    },
+  }
+})
+</script>
