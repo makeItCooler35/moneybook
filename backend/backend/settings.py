c303761e251c4d2dc7e843a096bb04f7c7f66570
Никита Богословский <nik35is@yandex.ru>
2023-09-08 14:59:49 +0300

    добавил библиотеку и путь к отчетам

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+REPORT_ROOT = str(BASE_DIR) + os.environ.get("REPORT_RELATIVE_ROOT", "/reports")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
